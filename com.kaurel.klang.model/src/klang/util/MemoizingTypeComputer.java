c0e660047d89bd64e45cff0adf2f0c079fef3303
Håkon Kaurel <kwrl@Hakons-MBP.lan>
2016-06-17 12:36:01 +0200

    Memoizing typecomputer and caching of Variable references added

---
+++ com.kaurel.klang.model/src/klang/util/MemoizingTypeComputer.java
@@ -0,0 +1,21 @@
+package klang.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.emf.ecore.EObject;
+
+public class MemoizingTypeComputer extends TypeComputer {
+	private Map<EObject, Class> cache = new HashMap<>();
+	
+	@Override
+	public Class doSwitch(EObject eObject) {
+		if(cache.containsKey(eObject)) {
+			return cache.get(eObject);
+		}
+		Class type = super.doSwitch(eObject);
+		cache.put(eObject, type);
+		
+		return type;
+	}
+}
